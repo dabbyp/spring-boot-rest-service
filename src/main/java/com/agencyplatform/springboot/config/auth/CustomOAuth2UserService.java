68dce94446135c2e62bf5b7b6d999a3a8648e7bc
dabbyp <60965242+dabbyp@users.noreply.github.com>
2020-03-15 17:51:52 +0900

    20200315 oauth2 연동완료

--- src/main/java/com/agencyplatform/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/agencyplatform/springboot/config/auth/CustomOAuth2UserService.java
@@ -28,15 +28,18 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+        // oauth2 로그인 진행중인 서비스 구분용 (구글/네이버 등)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        // oauth2 로그인 진행시 키가 되는 필드값을 의미 (구글:"sub")
+        // 네이버,카카오 등은 지원하지 않음
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
                 .getUserInfoEndpoint().getUserNameAttributeName();
-
+        // oauth2 로그인 성공 시, 사용자 정보를 담아온 클래스
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName,
                 oAuth2User.getAttributes());
-
+        // 특정속성(email 등)으로 사용자 정보를 업데이트 (구글 정보가 업데이트 되었을 경우, 동기화 목적)
         User user = saveOrUpdate(attributes);
-
+        // 세션에 사용자 정보를 저장하기 위한 Dto 클래스 (세션에 담기위해 직렬화가 필요하기 때문에 별도의 SessionUser  클래스를 사용)
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
--- src/main/java/com/agencyplatform/springboot/web/index/IndexController.java
+++ src/main/java/com/agencyplatform/springboot/web/index/IndexController.java
@@ -1,13 +1,32 @@
 package com.agencyplatform.springboot.web.index;
 
+import com.agencyplatform.springboot.config.auth.LoginUser;
+import com.agencyplatform.springboot.config.auth.dto.SessionUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String hello(){
+    public String index(Model model, @LoginUser SessionUser user){
+        //model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userEmail", user.getName() + "(" + user.getEmail() + ")" );
+            model.addAttribute("userPicture", user.getPicture() );
+        }
         return "index";
     }
+
+    @GetMapping("/index_stat")
+    public String index_stat(Model model, @LoginUser SessionUser user){
+        //model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userEmail", user.getName() + "(" + user.getEmail() + ")" );
+            model.addAttribute("userPicture", user.getPicture() );
+        }
+        return "index_stat";
+    }
+
 }
